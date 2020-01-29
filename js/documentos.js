d6c2883e4a8d52812c239aaf15a78d147b999c34
Vitor Cavalcante <vitorcavalcante997@gmail.com>
2020-01-29 20:57:19 -0300

    terminando projeto

--- js/documentos.js
+++ js/documentos.js
@@ -1,11 +1,71 @@
 function search(){
     var cpf = document.getElementById('inputCPF').value;
     console.log(cpf);
-   validation(cpf);
+    validation(cpf);
 }
-function validation(cpfvalue){
+function validation(cpfvalue){   
+    var storage = firebase.storage();
+    storage.ref().child(cpfvalue).listAll().then(function(todosArquivos){
+        console.log(todosArquivos.items);
+        
+        if(todosArquivos.items.length >= 1){
+            listFiles(cpfvalue);
+            next(cpfvalue);
 
+        } else {
+            alert('CPF não encontrado');
+        }
+
+    }).catch(function(error){
+        console.log('ERRO',error);
+    });
+
+}
+
+function listFiles(cpfvalue){
     document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: '+cpfvalue;
+    var storage = firebase.storage();
+    var arquivos;
+    var nomeArquivos = [];
+    var  linksArquivos = [];        
+    storage.ref().child(cpfvalue).listAll().then(function(todosArquivos){
+        arquivos = todosArquivos.items;
+    
+    for(let i=0; i<arquivos.length ;i++){
+        nomeArquivos.push(arquivos[i].name);
+
+        storage.ref(cpfvalue+'/'+nomeArquivos[i]).getDownloadURL().then(function(url){
+            var ul = document.getElementById("list");
+            var li = document.createElement("li");
+            var listItem = '<a href=" '+linksArquivos[i]+'"  target="_blank">'+nomeArquivos[i]+'...</a>';
+            li.innerHTML = listItem;
+            ul.appendChild(li);
+         
+           linksArquivos.push(url);
+        
+        }).catch(function(error){
+            console.log(error);
+        })
+      }
+    });
+    
+}
+
+
+function next(cpfvalue){
+    document.getElementById('busca').setAttribute("class","ocultar");
+    document.getElementById('resultado').removeAttribute("class","ocultar");
+    
 
 }
 
+function back(){
+
+    document.getElementById('busca').removeAttribute("class","ocultar");
+    document.getElementById('resultado').setAttribute("class","ocultar");
+    document.getElementById('inputCPF').value = '';
+
+
+
+
+}
---
+++ js/initfirebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBYslW8Eo0rphYaA3Zc-p4RQg8B5_75GSw",
+    authDomain: "documentos-17724.firebaseapp.com",
+    databaseURL: "https://documentos-17724.firebaseio.com",
+    projectId: "documentos-17724",
+    storageBucket: "documentos-17724.appspot.com",
+    messagingSenderId: "573957987628",
+    appId: "1:573957987628:web:fd7f9b1767aa3ad1676089"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
